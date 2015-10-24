5dc38f4d62d8c63420ab8334d7fda28441110071
Aran Nolan <nolanar@tcd.ie>
2015-10-24 12:15:16 +0100

    Change input to use command line argument

--- intermediate/236-fibonacci-ish/main.py
+++ intermediate/236-fibonacci-ish/main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""2015-10-14] Challenge #236 [Intermediate] Fibonacci-ish Sequence
+
+r/dailyprogrammer - https://redd.it/3opin7
+"""
+
+import sys
 
 def main():
-    target = int(input("Input: "))
+    target = int(sys.argv[1])
     for x in fib_gen(include_seed=False, bound=target):
         if target % x == 0:
             seed = target // x
@@ -10,6 +16,8 @@ def main():
         print(x)
 
 def fib_gen(include_seed=True, seed_value=1, bound=None):
+    """Fibonacci-like sequence generator
+    """
     a, b = 0, seed_value
     if include_seed:
         yield a
